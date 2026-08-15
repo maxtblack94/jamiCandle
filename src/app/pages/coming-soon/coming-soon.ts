a1da7efd31ad81837e820d15cd470e6397186847
Massimo Lanera <massimo.lanera@lutech.it>
2026-08-15 19:54:15 +0200

    admin da pagina coming soon

--- src/app/pages/coming-soon/coming-soon.ts
+++ src/app/pages/coming-soon/coming-soon.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-coming-soon',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './coming-soon.html',
   styleUrl: './coming-soon.scss',
 })
